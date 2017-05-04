7943aabf8a484af1dd541da7316183b94e041938
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2017-05-04 17:59:44 +0200

    Define more restrictions

--- dealii/matrixfree_laplace_dg.cc
+++ dealii/matrixfree_laplace_dg.cc
@@ -96,7 +96,7 @@ run(unsigned int grid_index, unsigned int refine, unsigned int degree)
 
   FEData<FE_DGQ, 1, 1, dim, 0, 1> fedata1(fe_u);
   FEDataFace<FE_DGQ, 1, 1, dim, 0, 1> fedata_face1(fe_u);
-  auto fe_datas = (fedata1, fedata_face1);
+  auto fe_datas = (fedata_face1, fedata1);
 
   std::vector<FiniteElement<dim>*> fes;
   fes.push_back(&fe_u);
@@ -112,7 +112,7 @@ run(unsigned int grid_index, unsigned int refine, unsigned int degree)
   auto f1 = form(Du, Dv);
   auto f2 = face_form(u_plus - u_minus, v_plus);
   auto f3 = face_form(u_minus - u_plus, v_minus);
-  auto f = /*f1 + */ f2 + f3;
+  auto f = f1 /*+ f2 + f3*/;
 
   MatrixFreeData<dim,
                  decltype(fe_datas),
